b0d3189e9997c41cea5992f421e8dadebdb72de5
Sofia Medina <sofi.k.medina@gmail.com>
2023-10-04 00:53:39 -0300

    deco 1
    Signed-off-by: Sofia Medina <sofi.k.medina@gmail.com>

--- src/main/java/org/skm/Main.java
+++ src/main/java/org/skm/Main.java
@@ -3,6 +3,7 @@ package org.skm;
 
 import org.skm.LifeChangingApp.LifeChangingApp;
 import org.skm.SimUDuck.*;
+import org.skm.StarbuzzCoffeeApp.StarbuzzCoffee;
 import org.skm.WeatherORama.WeatherOrama;
 
 import java.util.Scanner;
@@ -11,20 +12,22 @@ public class Main {
 
     public final static String PATTERN_STRATEGY = "PATTERN_STRATEGY";
     public final static String PATTERN_OBSERVER = "PATTERN_OBSERVER";
+    public final static String PATTERN_DECORATOR = "PATTERN_DECORATOR";
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("SELECT EG TO RUN:\n" +
-                "0:EXIT\n" +
-                "1:"+PATTERN_STRATEGY+"\n" +
-                "2:"+PATTERN_OBSERVER+"\n" +
-                "3:"+PATTERN_STRATEGY+ "-LIFE-CHANGING-APP\n"
-        );
-
-        String option_choose = sc.nextLine();
-//        String option_choose="2";
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("SELECT EG TO RUN:\n" +
+//                "0:EXIT\n" +
+//                "1:"+PATTERN_STRATEGY+"\n" +
+//                "2:"+PATTERN_OBSERVER+"\n" +
+//                "3:"+PATTERN_STRATEGY+ "-LIFE-CHANGING-APP\n"+
+//                "4:"+PATTERN_DECORATOR+"\n"
+//        );
+//
+//        String option_choose = sc.nextLine();
+        String option_choose="4";
 
         switch (option_choose) {
             case "0":
@@ -45,6 +48,12 @@ public class Main {
                 System.out.println("Running:" + PATTERN_OBSERVER + "-LIFE-CHANGING-APP");
                 LifeChangingApp lifeChangingApp = new LifeChangingApp();
                 lifeChangingApp.run();
+                break;
+
+            case "4":
+                System.out.println("Running:" + PATTERN_DECORATOR );
+                StarbuzzCoffee starbuzzCoffee = new StarbuzzCoffee();
+                starbuzzCoffee.run();
                 break;
 
             default:
---
+++ src/main/java/org/skm/StarbuzzCoffeeApp/StarbuzzCoffee.java
@@ -0,0 +1,11 @@
+package org.skm.StarbuzzCoffeeApp;
+
+public class StarbuzzCoffee {
+    public StarbuzzCoffee() {
+    }
+
+    public void run(){
+        System.out.println("running decorator example");
+    }
+
+}
