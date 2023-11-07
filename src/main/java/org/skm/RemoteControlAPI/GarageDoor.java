3c41141df0664f3e733bdc5b4d490842d547d077
Karina Sofia Medina <kamedina@expediagroup.com>
2023-11-06 22:51:59 -0300

    impl garage door open command
    Signed-off-by: Karina Sofia Medina <kamedina@expediagroup.com>

---
+++ src/main/java/org/skm/RemoteControlAPI/GarageDoor.java
@@ -0,0 +1,20 @@
+package org.skm.RemoteControlAPI;
+
+public class GarageDoor {
+    public void up(){
+        System.out.println("door going up");
+    }
+
+    public void down(){
+        System.out.println("door going down");
+    }
+    public void stop(){
+        System.out.println("stop door");
+    }
+    public void lightOn(){
+        System.out.println("garage door on");
+    }
+    public void lightOff(){
+        System.out.println("garage door off");
+    }
+}
